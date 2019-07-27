e31f7bff7669618730e0d0f7b89ba63dc8bc3687
Stephen Feuerstein <stephen@refactorstudios.com>
2019-07-27 10:13:35 -0600

    Initial project commit

---
+++ JSONTableView/JSONTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONTableView
+//
+//  Created by Stephen Feuerstein on 7/27/19.
+//  Copyright © 2019 Refactor Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
